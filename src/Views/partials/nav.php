980c69595d8e61ce2698f683ba944add927f31c0
okan-s <okan.simsek@chasacademy.se>
2019-01-15 09:32:35 +0100

    Changed header text & styling

--- src/Views/partials/nav.php
+++ src/Views/partials/nav.php
@@ -1,5 +1,5 @@
 <header class="header">
-    <h1>todos</h1>
+    <h1><span style="color:#149FA6">Todo<span style="color:#D0002E">MVC</span></h1>
     <form id="create-todo" method="post" action="todos">
       <input name="title" class="new-todo" placeholder="What needs to be done?" autofocus>
     </form>
